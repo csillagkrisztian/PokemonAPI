64f955accde2575f17efa7838b8e23b12557b6ed
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-10-13 14:29:07 +0200

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const getPokemonStats = require("./functions/getPokemonStats");
-const { response } = require("express");
 const cors = require("cors")
 const PORT = process.env.PORT || 4000;
 
